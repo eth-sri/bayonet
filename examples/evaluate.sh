07dbfbb1b5cbbd95a52939103a4ddec0543f9c68
Timon Gehr <timon.gehr@gmx.ch>
2017-01-24 00:56:50 +0100

    Add deterministic gossip.

--- examples/evaluate.sh
+++ examples/evaluate.sh
@@ -2,17 +2,16 @@
 
 # congestion
 ## random scheduling
-./run congestion-contracted.psi
-
-./run congestion-synthesis-contracted.psi
+./run.sh congestion-contracted.psi
+./run.sh congestion-synthesis-contracted.psi
 
 # ./run congestion-large-contracted.psi # TODO: make fast...
-# ./run congestion-large-synthesis-contracted.psi # PSI backend chokes on symbolic constraints. TODO: just use a better parameterization
+# ./run congestion-large-synthesis-contracted.psi # PSI backend chokes on symbolic constraints. TODO: use a better parameterization
 
 ## deterministic scheduling
 
-./run congestion-deterministic-contracted.psi  # always congested
-./run congestion-deterministic2-contracted.psi # prob of congestion 1/8, but scheduler is hard to explain
+./run.sh congestion-deterministic-contracted.psi  # always congested
+./run.sh congestion-deterministic2-contracted.psi # prob of congestion 1/8, but scheduler is hard to explain
 
 # ./run congestion-synthesis-deterministic-contracted.psi # seems pointless
 
@@ -21,7 +20,7 @@
 # reliability
 
 ## random scheduling
-./run reliability-contracted.psi
+./run.sh reliability-contracted.psi
 
 ## deterministic scheduling
 # unnecessary: result is independent of scheduling as we model only one packet at a time
@@ -29,8 +28,12 @@
 # Gossip
 
 ## random scheduling
-# N/A
+./run.sh gossip-contracted.psi
 
 ## deterministic scheduling
+./run.sh gossip-deterministic-contracted.psi
 
-# TODO!
+
+
+# moved to end because large:
+./run.sh congestion-large-contracted.psi
