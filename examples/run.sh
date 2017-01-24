ffd525a4d8e80887cef26236a14a486fe0b22afd
Timon Gehr <timon.gehr@gmx.ch>
2017-01-24 01:34:57 +0100

    Improve run.sh such that it skips over existing results.

--- examples/run.sh
+++ examples/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 PSI="psi --noboundscheck --trace --bruteforce --mathematica"
 
-{ { time $PSI $1; } 2>&1; } > ./results/${1%.psi}.txt
+if [ ! -f ./results/${1%.psi}.txt ]; then
+    { { time $PSI $1; } 2>&1; } > ./results/${1%.psi}.txt
+else
+    echo skipping $1.
+fi
