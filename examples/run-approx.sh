fe8110b2dda4c151e7f834ce781da2920172862d
Timon Gehr <timon.gehr@gmx.ch>
2018-02-27 15:50:06 +0100

    Print messages during evaluation.

--- examples/run-approx.sh
+++ examples/run-approx.sh
@@ -4,6 +4,7 @@ PSI="./psi --noboundscheck --trace --webppl --expectation --mathematica"
 
 cd ../../psi-approximate/
 if [ ! -f ../bayonet-implementation/examples/results-approx/${1%.psi}.txt ]; then
+    echo running $1
     { { time $PSI ../bayonet-implementation/examples/$1; } 2>&1; } > ../bayonet-implementation/examples/results-approx/${1%.psi}.txt
 else
     echo skipping $1.
