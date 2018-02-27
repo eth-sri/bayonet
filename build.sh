6c568f2a158d1689dcfb2c60848e16715b03b3b7
Timon Gehr <timon.gehr@gmx.ch>
2018-02-27 17:23:41 +0100

    Add build script.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    BIN="linux/bin64"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    BIN="osx/bin"
+fi
+
+if [[ -d "dmd2" ]]; then
+    DMD="./dmd2/$BIN/dmd"
+else
+    DMD="dmd"
+fi
+
+# debug build
+$DMD -g -debug *.d -ofbayonet
