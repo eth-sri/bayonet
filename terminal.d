c189dc9d2692cb4d1a0d1ef19c6af628461c56cb
Timon Gehr <timon.gehr@gmx.ch>
2018-03-18 20:15:39 +0100

    Fix.

--- terminal.d
+++ terminal.d
@@ -1,7 +1,8 @@
-// Written in the D programming language.
+// Written in the D programming language
 
 import std.stdio;
 import core.stdc.stdlib;
+import core.stdc.string;
 
 enum CSI = "\033[";
 enum RESET=CSI~"0m";
@@ -19,12 +20,11 @@ version(linux){
 	private extern(C) size_t isatty(size_t desc);
 	private extern(C) int fileno(shared(_iobuf)*);
 	bool isATTy(ref File f){ // determine whether a given file is connected to a terminal
-		if(getenv("EMACS")) return false;
+		if(!strcmp(getenv("TERM"),"dumb")) return false;
 		return cast(bool)isatty(fileno(f.getFP()));
 	}
 	int getTabSize(){
-		if(getenv("EMACS")) return 4;
-		return 8;
+		return 4;
 	}
 }else{
 	bool isATTy(ref File){return false;}
