7bd4b217b5b0b315551ad3aca9dc17ae6b8a1100
Keyur Joshi <kpjoshi2@illinois.edu>
2018-12-07 11:47:31 -0600

    modified some benchmarks

--- keyur/genNodes6.py
+++ keyur/genNodes6.py
@@ -7,7 +7,7 @@ def printLink(n1,p1,n2,p2,comma=True):
   commaStr = "," if comma else ""
   print("    (",n1,",pt",p1,") <-> (",n2,",pt",p2,")",commaStr,sep="")
 
-print("num_steps",(n+1)*24,";")
+print("num_steps",(n+1)*18,";")
 
 print("topology {\n  nodes { H0, H1", end="")
 for i in range(n*4):
@@ -17,15 +17,14 @@ printLink("H0",1,"S0",1)
 print()
 for i in range(n):
   if i>0:
-    printLink("S"+str(4*i-3),3,"S"+str(4*i+0),1)
+    printLink("S"+str(4*i-1),3,"S"+str(4*i+0),1)
     print()
   printLink("S"+str(4*i+0),2,"S"+str(4*i+1),1)
   printLink("S"+str(4*i+0),3,"S"+str(4*i+2),1)
-  printLink("S"+str(4*i+0),4,"S"+str(4*i+3),1)
-  printLink("S"+str(4*i+3),2,"S"+str(4*i+2),3)
-  printLink("S"+str(4*i+2),2,"S"+str(4*i+1),2)
+  printLink("S"+str(4*i+1),2,"S"+str(4*i+3),1)
+  printLink("S"+str(4*i+2),2,"S"+str(4*i+3),2)
   print()
-printLink("S"+str(4*n-3),3,"H1",1,False)
+printLink("S"+str(4*n-1),3,"H1",1,False)
 print("  }\n}\nprograms { H0 -> h0, H1 -> h1",end="")
 for i in range(n):
   print(", S",i*4," -> s0, S",i*4+1," -> s1, S",i*4+2," -> s2, S",i*4+3," -> s3",sep="",end="")
